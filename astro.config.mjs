f3b852722292914abeb6f83558a1916e8f8fe479
Zzz1750 <antonyzharon2@gmail.com>
2025-11-23 22:45:35 +0530

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://zzz1750.github.io/TAG-BREAK/",
+  outDir: "dist",
+});
