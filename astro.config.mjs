5d0d97c18136e3a725014dc944bf723c6eecc8d2
Zzz1750 <antonyzharon2@gmail.com>
2026-01-13 16:06:05 +0530

    url update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,3 @@
-// import { defineConfig } from 'astro/config';
-
-// export default defineConfig({
-//   site: 'https://zzz1750.github.io/TAG-BREAK/',
-//   base: '/TAG-BREAK/', 
-// });
-
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
